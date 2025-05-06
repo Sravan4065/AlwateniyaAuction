18346c2140ba2e6a4cb445cb94c9bca49668b41c
GDCIT-LAPT047\Sai Sravan.G <sravansai9312@gmail.com>
2025-05-06 15:39:30 +0530

    Added new screens for with and without trade for seller and buyer in register2 form, added paymentMethod screen for buyer type.

--- AlwataneyaAuctionExport01mayaftr/AlwataneyaAuction/controllers/mobile/frmPaymentMethodController/frmPaymentMethodController.js
+++ AlwataneyaAuctionExport01mayaftr/AlwataneyaAuction/controllers/mobile/frmPaymentMethodController/frmPaymentMethodController.js
@@ -1,5 +1,12 @@
 define({ 
 
  //Type your controller code here 
-
+  onNavigate: function(){
+    this.view.btnSkip.onClick= this.btnSkipOnClickAction;
+  },
+  
+  btnSkipOnClickAction: function(){
+    var ntf = new voltmx.mvc.Navigation("frmDashBoard");
+    ntf.navigate();
+  }
  });
